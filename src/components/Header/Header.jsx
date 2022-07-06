fbe61a36192650fba4811d41d356eaa8a5c91f5a
ubaidmalik0303 <ubaidmalik0303@gmail.com>
2022-07-06 14:23:53 +0500

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderStyles from "./header.module.css";
+
+const Header = () => {
+  return (
+    <header className={HeaderStyles.header}>
+      <h1>VUEEXE</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/VideoBanner/VideoBanner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoBannerStyles from "./videobanner.module.css";
+
+const VideoBanner = () => {
+  return(
+    <>
+        
+    </>
+  );
+};
+
+export default VideoBanner;
